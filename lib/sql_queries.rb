963fc2821d0e1eacf2f57ac43ebf0c701be5ab6e
Kristen Clunie <kcluniesa@yahoo.com>
2019-02-17 18:42:15 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -28,7 +28,7 @@ ON projects.id = pledges.project_id GROUP BY (projects.title) HAVING SUM(pledges
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"SELECT users.name, pledges.amount
+"SELECT users.name, SUM(pledges.amount) 
 FROM projects 
 INNER JOIN pledges 
 ON projects.id = pledges.user_id GROUP BY (users.name) ORDER BY (SUM(pledges.amount)), users.name"

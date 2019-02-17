ea5dd452f6c7f59e7c3a8797f4e447fa58b9b8ee
Kristen Clunie <kcluniesa@yahoo.com>
2019-02-17 18:44:44 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -36,7 +36,7 @@ end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
 "SELECT projects.category, pledges.amount
-FROM users 
+FROM projects 
 INNER JOIN pledges 
 ON users.id = pledges.user_id GROUP BY (users.name)"
 end

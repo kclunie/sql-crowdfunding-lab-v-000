98ad68a2c61adc6078cdab6b5350e599e9a4331f
Kristen Clunie <kcluniesa@yahoo.com>
2019-02-17 16:35:42 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -3,18 +3,17 @@ id INTEGER PRIMARY KEY,
 title TEXT,
 category TEXT,
 funding_goal TEXT,
-start_date, TEXT,
-end_date, TEXT
+start_date TEXT,
+end_date TEXT
 );
 
-CREATE TABLE cats (
+CREATE TABLE user (
 id INTEGER PRIMARY KEY,
 name TEXT,
-age INTEGER,
-breed TEXT
+age INTEGER
 );
 
-CREATE TABLE cats (
+CREATE TABLE pledge (
 id INTEGER PRIMARY KEY,
 name TEXT,
 age INTEGER,
